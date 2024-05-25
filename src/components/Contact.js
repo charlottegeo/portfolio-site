9a399eb8e49d47229f85e38ca0cf61d760f9defa
Charlotte George <cngg805@gmail.com>
2024-05-25 16:45:19 -0400

    Added initial React files

---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact</h2>
+      <p>Email: cngg805@gmail.com</p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Projects() {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      <ul>
+        <li>CatJam</li>
+        <li>Devcade Contributions</li>
+        <li>GenQ</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Projects;
